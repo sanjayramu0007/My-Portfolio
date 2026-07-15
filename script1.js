92476c85108ebe6e9642e9558dc77cfed4ce0dcc
sanjayramu0007 <sanjayramu0007@gmail.com>
2026-07-15 12:45:55 +0530

    Delete script1.js

--- script1.js
+++
@@ -1,3 +0,0 @@
-
-    tailwind.config={darkMode:'class',theme:{extend:{fontFamily:{display:['PT Sans','sans-serif'],body:['DM Sans','sans-serif']},colors:{accent:'#FF6B2B','accent-light':'#FF8F5C'}}}}
-  
