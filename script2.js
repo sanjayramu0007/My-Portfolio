446f1db59634b3f2fbc8a07e094b1082eae779de
sanjayramu0007 <sanjayramu0007@gmail.com>
2026-07-15 12:46:10 +0530

    Delete script2.js

--- script2.js
+++
@@ -1,6 +0,0 @@
-
-    document.getElementById('year').textContent = new Date().getFullYear();
-    const observer = new IntersectionObserver(entries => {
-      entries.forEach(e => { if(e.isIntersecting){e.target.classList.add('visible');observer.unobserve(e.target);} });
-    },{threshold:.1,rootMargin:'0px 0px -40px 0px'});
-    document.querySelectorAll('.reveal').forEach(el=>observer.observe(el));
